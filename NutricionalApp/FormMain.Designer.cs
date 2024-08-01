27da419bf42c4739789fee5312e99c040936b2e9
Vicenzo <vicenzofrk@gmail.com>
2024-07-31 21:55:39 -0300

    Adicionar arquivos de projeto.

---
+++ NutricionalApp/FormMain.Designer.cs
@@ -0,0 +1,158 @@
+﻿namespace NutricionalApp
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormMain));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.bt_resize = new System.Windows.Forms.Button();
+            this.bt_Exit = new System.Windows.Forms.Button();
+            this.bt_login = new System.Windows.Forms.Button();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(35)))), ((int)(((byte)(39)))), ((int)(((byte)(42)))));
+            this.panel1.Controls.Add(this.panel3);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(200, 511);
+            this.panel1.TabIndex = 10;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(195, 37);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "NutricionalAPP";
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(35)))), ((int)(((byte)(39)))), ((int)(((byte)(42)))));
+            this.panel2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.panel2.Controls.Add(this.bt_login);
+            this.panel2.Controls.Add(this.bt_resize);
+            this.panel2.Controls.Add(this.bt_Exit);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Right;
+            this.panel2.Location = new System.Drawing.Point(1132, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(52, 511);
+            this.panel2.TabIndex = 11;
+            // 
+            // bt_resize
+            // 
+            this.bt_resize.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.bt_resize.FlatAppearance.BorderSize = 0;
+            this.bt_resize.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.bt_resize.Image = ((System.Drawing.Image)(resources.GetObject("bt_resize.Image")));
+            this.bt_resize.Location = new System.Drawing.Point(0, 421);
+            this.bt_resize.Name = "bt_resize";
+            this.bt_resize.Size = new System.Drawing.Size(52, 45);
+            this.bt_resize.TabIndex = 13;
+            this.bt_resize.UseVisualStyleBackColor = true;
+            this.bt_resize.Click += new System.EventHandler(this.bt_resize_Click);
+            // 
+            // bt_Exit
+            // 
+            this.bt_Exit.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.bt_Exit.FlatAppearance.BorderSize = 0;
+            this.bt_Exit.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.bt_Exit.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.bt_Exit.Image = ((System.Drawing.Image)(resources.GetObject("bt_Exit.Image")));
+            this.bt_Exit.Location = new System.Drawing.Point(0, 466);
+            this.bt_Exit.Name = "bt_Exit";
+            this.bt_Exit.Size = new System.Drawing.Size(52, 45);
+            this.bt_Exit.TabIndex = 12;
+            this.bt_Exit.UseVisualStyleBackColor = true;
+            this.bt_Exit.Click += new System.EventHandler(this.bt_Exit_Click);
+            // 
+            // bt_login
+            // 
+            this.bt_login.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.bt_login.FlatAppearance.BorderSize = 0;
+            this.bt_login.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.bt_login.Image = ((System.Drawing.Image)(resources.GetObject("bt_login.Image")));
+            this.bt_login.Location = new System.Drawing.Point(0, 376);
+            this.bt_login.Name = "bt_login";
+            this.bt_login.Size = new System.Drawing.Size(52, 45);
+            this.bt_login.TabIndex = 14;
+            this.bt_login.UseVisualStyleBackColor = true;
+            // 
+            // panel3
+            // 
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel3.Location = new System.Drawing.Point(0, 37);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(200, 474);
+            this.panel3.TabIndex = 1;
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(1184, 511);
+            this.ControlBox = false;
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.IsMdiContainer = true;
+            this.MaximizeBox = false;
+            this.Name = "FormMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Nutricional";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button bt_resize;
+        private System.Windows.Forms.Button bt_Exit;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button bt_login;
+        private System.Windows.Forms.Panel panel3;
+    }
+}
+
---
+++ NutricionalApp/FormMain.cs
@@ -0,0 +1,59 @@
+﻿using Npgsql;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NutricionalApp
+{
+    public partial class FormMain : Form
+    {
+        public FormMain()
+        {
+            InitializeComponent();
+            TestCon();
+        }
+
+        private static NpgsqlConnection GetConnection()
+        {
+            return new NpgsqlConnection(@"Server=localhost,Port=5432,User Id=postgres;Password=mASTERKEY;Database=Nutricional;");
+        }
+        
+        private static void TestCon()
+        {
+            using(NpgsqlConnection con = GetConnection())
+            {
+                con.Open();
+                if (con.State==ConnectionState.Open)
+                {
+                    Console.WriteLine("Conectado!!");
+                }
+            }
+        }
+
+        private void bt_Exit_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("Deseja Sair?", "Sair", MessageBoxButtons.YesNo) == DialogResult.Yes)
+            {
+                    this.Close();
+            }
+        }
+
+        private void bt_resize_Click(object sender, EventArgs e)
+        {
+            if (this.WindowState == FormWindowState.Normal)
+            {
+                this.WindowState = FormWindowState.Maximized;
+            }
+            else if (this.WindowState == FormWindowState.Maximized)
+            {
+                this.WindowState = FormWindowState.Normal;
+            }
+        }
+    }
+}
